beee51c0e04c7fc89403b4c9dfd960e0696005b5
Cloud <65218174+cloudkasalu@users.noreply.github.com>
2024-07-25 23:40:09 +0200

    navigation, homepage, search and read article updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,31 @@
 const drawerTogglers = document.querySelectorAll('.drawer-toggler')
 const drawer = document.querySelector('.drawer');
+const drawerOverlay = document.querySelector('.drawer-overlay');
 const dropdowns = document.querySelectorAll('.dropdown');
 const subDropdowns = document.querySelectorAll('.sub-dropdown');
 
 
+function toggleDrawer(toggler){
+
+    const isExpanded = toggler.getAttribute('aria-expanded') === 'true';
+      
+    document.body.classList.toggle('no-scroll', !isExpanded);
+
+    drawer.classList.toggle('show', !isExpanded);
+    drawerTogglers.forEach((toggler) => {
+        toggler.setAttribute('aria-expanded', String(!isExpanded));
+    })
+}
 
 drawerTogglers.forEach(function(toggler){
-
-    toggler.addEventListener('click', (e) => {
-        console.log("Hello")
-        const isExpanded = toggler.getAttribute('aria-expanded') === 'true';
-      
-        document.body.classList.toggle('no-scroll', !isExpanded);
-    
-        drawer.classList.toggle('show', !isExpanded);
-        drawerTogglers.forEach((toggler) => {
-            toggler.setAttribute('aria-expanded', String(!isExpanded));
-        })
-      });
+    toggler.addEventListener('click', (e)=>{
+        toggleDrawer(toggler);
+    });
 })
 
+drawerOverlay.addEventListener('click',(e)=>{
+    toggleDrawer(drawerTogglers[0])
+})
 
 dropdowns.forEach((dropdown)=>{
     let button = dropdown.querySelector('.dropdown-button');
@@ -40,7 +46,9 @@ dropdowns.forEach((dropdown)=>{
         }
 
         dropdowns.forEach((dropdown)=>{
-            dropdown.classList.remove('active')
+            if(!dropdown.classList.contains('profile')){
+                dropdown.classList.remove('active')
+            }
         })
         
         dropdown.classList.add('active')
